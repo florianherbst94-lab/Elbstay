1104ac1a2e15d2148229f020c0c99f3b1dc0c899
Florian Herbst <florianherbst@MBP-von-Florian.local>
2026-07-03 19:34:57 +0200

    feat: Add full high-res photo gallery for Boutique-Apartment

--- src/lib/boutique/images.ts
+++ src/lib/boutique/images.ts
@@ -4,10 +4,63 @@ export const boutiqueGallery: ImageCategory[] = [
   {
     title: "Wohnbereich",
     images: [
-      "/images/boutique/living-room-4.jpg",
-      "/images/boutique/living-room-3.jpg",
-      "/images/boutique/living-room-2.jpg",
-      "/images/boutique/living-room-1.jpg"
+      "/images/boutique/01_wohnbereich_sofa_kunst_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/02_wohnzimmer_essbereich_weit_airbnb_edit_10mb.jpg",
+      "/images/boutique/03_wohnzimmer_sofa_detail_airbnb_edit_10mb.jpg"
+    ]
+  },
+  {
+    title: "Essbereich",
+    images: [
+      "/images/boutique/02_essbereich_tv_hoch_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/03_essbereich_tv_weit_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/05_esstisch_detail_01_airbnb_edit_10mb.jpg",
+      "/images/boutique/06_esstisch_detail_02_airbnb_edit_10mb.jpg",
+      "/images/boutique/07_esstisch_detail_03_airbnb_edit_10mb.jpg",
+      "/images/boutique/10_studio_essbereich_bett_airbnb_style_HIGHRES_8-10MB.jpg"
+    ]
+  },
+  {
+    title: "Küche",
+    images: [
+      "/images/boutique/07_kueche_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/10_kueche_gesamt_airbnb_edit_10mb.jpg",
+      "/images/boutique/04_kuechengeraete_detail_airbnb_edit_10mb.jpg"
+    ]
+  },
+  {
+    title: "Schlafbereich",
+    images: [
+      "/images/boutique/04_schlafbereich_weit_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/05_schlafbereich_detail_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/06_schlafbereich_front_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/07_schlafbereich_portrait_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/08_schlafbereich_portrait2_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/09_schlafbereich_fenster_airbnb_style_HIGHRES_8-10MB.jpg",
+      "/images/boutique/09_schlafzimmer_front_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/10_schlafzimmer_weit_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/02_spiegel_schlafzimmer_reflexion_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/03_spiegel_schlafzimmer_detail_airbnb_style_HIGHRES.jpg"
+    ]
+  },
+  {
+    title: "Bad",
+    images: [
+      "/images/boutique/01_bad_gesamt_01_airbnb_edit_highres.jpg",
+      "/images/boutique/08_bad_gesamt_02_airbnb_edit_highres.jpg",
+      "/images/boutique/03_badewanne_01_airbnb_edit_highres.jpg",
+      "/images/boutique/06_badezimmer_badewanne_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/04_badezimmer_regal_deko_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/05_badezimmer_waschbecken_airbnb_style_HIGHRES.jpg",
+      "/images/boutique/02_bad_detail_waschbecken_seifenspender_airbnb_edit_highres.jpg"
+    ]
+  },
+  {
+    title: "Flur & Durchgang",
+    images: [
+      "/images/boutique/01_flur_bad_durchgang_airbnb_edit_10mb.jpg",
+      "/images/boutique/09_flur_gesamt_airbnb_edit_highres.jpg",
+      "/images/boutique/10_flur_detail_spiegel_airbnb_edit_highres.jpg"
     ]
   }
 ];
