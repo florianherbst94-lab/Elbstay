53b3d0f4d57db4a8d0a5e9b210a586b1dda1eae4
Florian Herbst <florianherbst@MBP-von-Florian.local>
2026-09-26 20:01:42 +0200

    Add full high-res image gallery for boutique-2 from desktop

--- src/lib/boutique-2/images.ts
+++ src/lib/boutique-2/images.ts
@@ -4,33 +4,105 @@ export const boutique2Gallery: ImageCategory[] = [
   {
     "title": "Highlights",
     "images": [
-      "/images/boutique-2/image-1.png",
-      "/images/boutique-2/image-2.png"
+      "/images/boutique-2/01_Esszimmer.png",
+      "/images/boutique-2/01_Schlafzimmer.png",
+      "/images/boutique-2/02_Bad_Waschtisch.png",
+      "/images/boutique-2/03_Badezimmer.png"
     ]
   },
   {
-    "title": "Wohnbereich",
+    "title": "Badezimmer 1",
     "images": [
-      "/images/boutique-2/image-3.png",
-      "/images/boutique-2/image-4.png"
+      "/images/boutique-2/02_Bad_Waschtisch.png",
+      "/images/boutique-2/03_Bad_Waschbereich.png",
+      "/images/boutique-2/04_Badewanne.png",
+      "/images/boutique-2/05_Bad_Gesamtansicht.png",
+      "/images/boutique-2/05_Badewanne_Detail.png",
+      "/images/boutique-2/06_Bad_Gegenansicht.png",
+      "/images/boutique-2/06_Badezimmer.png"
+    ]
+  },
+  {
+    "title": "Badezimmer 2",
+    "images": [
+      "/images/boutique-2/03_Badezimmer.png",
+      "/images/boutique-2/04_Dusche.png",
+      "/images/boutique-2/04_Waschtisch_Detail.png"
+    ]
+  },
+  {
+    "title": "Flur",
+    "images": [
+      "/images/boutique-2/02_Flur_Schrank.png",
+      "/images/boutique-2/05_Flur.png"
+    ]
+  },
+  {
+    "title": "Küche",
+    "images": [
+      "/images/boutique-2/04_Loftkueche_Dachfenster.png",
+      "/images/boutique-2/05_Kuechenzeile_Philips.png",
+      "/images/boutique-2/06_Kuechenvignette_Teebox_Messerblock.png",
+      "/images/boutique-2/07_Kueche_Philips_Geraete.png",
+      "/images/boutique-2/07_Spuelmaschine.png",
+      "/images/boutique-2/08_Galley_Kueche.png",
+      "/images/boutique-2/08_Kochgeschirr.png",
+      "/images/boutique-2/09_Geschirr_Kuechenschrank.png",
+      "/images/boutique-2/10_Glasgeschirr_Schrank.png"
     ]
   },
   {
     "title": "Schlafzimmer 1",
     "images": [
-      "/images/boutique-2/image-5.png"
+      "/images/boutique-2/01_Schlafzimmer.png",
+      "/images/boutique-2/01_Schlafzimmerecke_Pflanze.png",
+      "/images/boutique-2/02_Dachzimmer_Bett_frontal.png",
+      "/images/boutique-2/06_Schlafzimmer_Detail.png",
+      "/images/boutique-2/07_Schlafzimmer_TV.png",
+      "/images/boutique-2/08_Schlafzimmer_Gesamt.png",
+      "/images/boutique-2/09_Buegeleisen_Buegelbrett.png",
+      "/images/boutique-2/09_Kleiderschrank_Ausstattung.png",
+      "/images/boutique-2/10_Schlafzimmer_TV.png",
+      "/images/boutique-2/10_TV_Bereich.png"
     ]
   },
   {
     "title": "Schlafzimmer 2",
     "images": [
-      "/images/boutique-2/image-6.png"
+      "/images/boutique-2/03_Dachfenster_Bett_Detail.png",
+      "/images/boutique-2/04_Ausblick_Dachfenster.png",
+      "/images/boutique-2/05_Schlafzimmer_Olivenmotiv.png",
+      "/images/boutique-2/06_Schlafzimmer_Netflix_TV.png",
+      "/images/boutique-2/07_Schlafzimmer_Velux.png",
+      "/images/boutique-2/08_Dachzimmer_Gesamtansicht.png"
     ]
   },
   {
-    "title": "Badezimmer",
+    "title": "Schlafzimmer 3",
     "images": [
-      "/images/boutique-2/image-7.png"
+      "/images/boutique-2/09_Murphy_Bett.png",
+      "/images/boutique-2/10_Boutique_Schlafzimmer.png"
+    ]
+  },
+  {
+    "title": "Schlafzimmer 4",
+    "images": [
+      "/images/boutique-2/01_Dachzimmer_Handtuecher.png",
+      "/images/boutique-2/02_Loft_Esstisch.png"
+    ]
+  },
+  {
+    "title": "Wohnbereich",
+    "images": [
+      "/images/boutique-2/01_Esszimmer.png",
+      "/images/boutique-2/01_Wohnzimmer.png",
+      "/images/boutique-2/02_Essbereich.png",
+      "/images/boutique-2/03_Esstisch_Hochformat.png",
+      "/images/boutique-2/03_Sideboard_Spielesammlung.png",
+      "/images/boutique-2/07_Essbereich.png",
+      "/images/boutique-2/08_Wohn_Essbereich.png",
+      "/images/boutique-2/09_Wohnung_Uebersicht.png",
+      "/images/boutique-2/10_Esstisch_Detail.png"
     ]
   }
 ];
